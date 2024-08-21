4d548c5bdfb40479f6369f25cd03b0ffa351b2ab
Pendyala Divya <158457106+Divvu0717@users.noreply.github.com>
2024-08-21 11:06:13 +0000

    divya added pswd

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -40,3 +40,4 @@ has_number = input("Do you want to have numbers (y/n)? ").lower() == "y"
 has_special = input("Do you want to have special characters (y/n)? ").lower == "y"
 pwd = generate_password(min_length, has_number, has_special)
 print("The generated password is:", pwd)
+print("Password generator")
